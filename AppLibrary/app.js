ab519547e326e52ac7b34e0e3be5214dc36f46c6
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2022-06-27 18:40:40 +0800

    App Server

--- AppLibrary/app.js
+++ AppLibrary/app.js
@@ -9,7 +9,7 @@ const corsOptions = {
 };
 app.use(bodyParser.json());
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin',corsOptions.origin);
+    res.setHeader('Access-Control-Allow-Origin', corsOptions.origin);
     res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
--- AppLibrary/controllers/auth.js
+++ AppLibrary/controllers/auth.js
@@ -8,18 +8,14 @@ exports.signup = async (req,res,next)=>{
     const email = req.body.email;
     const password = req.body.password;
     try {
-        const hashedPassword = await bcrypt.hash(password, 12)
-        const userDetails = {
-            name: name,
-            email: email,
-            password: hashedPassword
-        }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const userDetails = { name: name, email: email, password: hashedPassword };
         const result = await User.save(userDetails);
-        res.status(201).json({message: 'User registered!'})
+        res.status(201).json({message: 'User registered!'});
     } catch (err) {
         if(!err.statusCode){
             err.statusCode = 500;
         }
         next(err);
     }
-}
+};
