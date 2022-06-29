40c3aacea88700c9b6e88bca2b843da5ad966790
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2022-06-30 01:15:25 +0800

    fix library

--- AppLibrary/app.js
+++ AppLibrary/app.js
@@ -1,24 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const PORT = process.env.PORT || 8080;
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+
 app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to denr human resource website." });
 });
 require("./routes/tutorial.routes.js")(app);
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
