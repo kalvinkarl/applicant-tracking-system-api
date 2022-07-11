d24b1ad1b970be8e0e680d7ed6da8be806159828
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2022-07-12 00:28:10 +0800

    test

--- AppServer/App.js
+++ AppServer/App.js
@@ -10,11 +10,17 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
+	res.setHeader("Access-Control-Allow-Methods",process.env.METHODS);
+	res.setHeader("Access-Control-Allow-Headers",process.env.HEADERS);
+	next();
+});
 app.get("/", (req, res) => res.json({ message: "Welcome to denr human resource website api." }));
-//Users
-require("./routes/user.routes")(app);
-//Admin
-require("./routes/admin/applicant.routes")(app);
+// //Users
+// require("./routes/user.routes")(app);
+// //Admin
+// require("./routes/admin/applicant.routes")(app);
 app.get("*", (req, res) => res.json({ error: "page not found" }));
 app.listen(PORT, () => console.log("Server is running on port:"+PORT));
 module.exports.handler = serverless(app);
