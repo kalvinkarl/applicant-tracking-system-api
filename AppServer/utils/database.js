61fd6cc8a564d6be88f444cc4be436b91f171251
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2022-07-12 20:21:26 +0800

    off database

--- AppServer/utils/database.js
+++ AppServer/utils/database.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2");
 const config = require("../config/config.json");
 // Create a connection to the database
-const connection = mysql.createConnection({
-	host: config.host,
-	user: config.user,
-	password: config.password,
-	database: config.database
-});
+// const connection = mysql.createConnection({
+// 	host: config.host,
+// 	user: config.user,
+// 	password: config.password,
+// 	database: config.database
+// });
 // Open the MySQL connection
-connection.connect(error => {
-	if (error) throw error;
-	console.log("Database is connected on database:"+config.database);
-});
-module.exports = connection;
+// connection.connect(error => {
+// 	if (error) throw error;
+// 	console.log("Database is connected on database:"+config.database);
+// });
+// module.exports = connection;
