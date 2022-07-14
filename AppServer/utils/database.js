992ee79443731d8e18a51890b1850ba81f4209fe
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2022-07-14 15:36:28 +0800

    RDS mysql

--- AppServer/utils/database.js
+++ AppServer/utils/database.js
@@ -1,25 +1,15 @@
 const mysql = require("mysql2");
 const config = require("../config/config.json");
-// Create a pool to the database connection
-const connection = mysql.createPool({
+// Create a connection to the database
+const connection = mysql.createConnection({
 	host: config.host,
 	user: config.user,
 	password: config.password,
 	database: config.database,
 	port: config.port
 });
-
-// Create a connection to the database
-// const connection = mysql.createConnection({
-// 	host: config.host,
-// 	user: config.user,
-// 	password: config.password,
-// 	database: config.database,
-// 	port: config.port
-// });
-
 // Open the MySQL connection
-connection.getConnection(error => {
+connection.connect(error => {
 	if (error) throw error;
 	console.log("Database is connected on database:"+config.database);
 });
