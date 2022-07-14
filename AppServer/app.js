9ad0730a1ced8c2dddac071d8174918b746ada18
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2022-07-14 14:37:25 +0800

    AppServer

--- AppServer/app.js
+++ AppServer/app.js
@@ -1,11 +1,16 @@
 const config = require("./config/config.json");
 const serverless = require("serverless-http");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = config.env.PORT;
+var corsOptions = {
+  origin: "*"
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/home", (req, res) => res.json({ message: "Welcome to denr human resource website api." }));
+app.get("/", (req, res) => res.json({ message: "Welcome to denr human resource website api." }));
 //Users
 require("./routes/user.routes")(app);
 //Admins
