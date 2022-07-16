d2e167a76dddbbdfa3175a95e71f8d15e45f7f4e
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2022-07-17 02:58:10 +0800

    private origin

--- AppServer/app.js
+++ AppServer/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 const PORT = config.env.PORT;
 var corsOptions = {
-  origin: "*"
+  origin: config.env.ORIGIN
 };
 app.use(cors(corsOptions));
 app.use(express.json());
